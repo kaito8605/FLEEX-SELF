7e27b9ff61f1b850b015bae23dd5026f8964fb17
kaito8605 <60184970+kaito8605@users.noreply.github.com>
2020-02-17 17:30:53 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+const token = "{ton token}" //ton token 
+const prefix = "{ton prefix}" //ton prefix
+const color = "{ta couleur}" //votre code couleur exemple de couleur #fc1b00
+
+
+
+
+
+
+module.exports = {
+	token,
+	prefix,
+    color
+}
